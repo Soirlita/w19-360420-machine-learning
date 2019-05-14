c328a833f78926b06b7f295957b97cac6d9a45b2
Soirlita <solam58@users.noreply.github.com>
2019-05-14 09:46:47 -0400

    I tried

--- kNNMain.java
+++ kNNMain.java
@@ -2,22 +2,30 @@ import java.util.List;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 
-
+//Its on GitHub
 
 public class kNNMain{
 
-  public static void main(String... args) throws FileNotFoundException{
+  public static void main(String... args) throws FileNotFoundException
+  {
 
     // TASK 1: Use command line arguments to point DataSet.readDataSet method to
     // the desired file. Choose a given DataPoint, and print its features and label
-
-
+	
+	List<DataPoint> excel = DataSet.readDataSet(args[0]); 
+	DataPoint dp = excel.get(0); // 0 = First data point list
+	double [] features = dp.x; // x is the array of datapoint we chose
+	double [] array = Arrays.copyOfRange(features,1,features.length);
+	System.out.println(Arrays.toString(array));
+	
 
     //TASK 2:Use the DataSet class to split the fullDataSet into Training and Held Out Test Dataset
-
-
-    // TASK 3: Use the DataSet class methods to plot the 2D data (binary and multi-class)
-
+	
+	double fractionTrainingSet = 0.2;
+	double fractionTestSet = 1.0 - fractionTrainingSet;
+	
+	List<DataPoint> testSet = DataSet.getTestSet(excel, fractionTestSet);
+	List<DataPoint> trainingSet = DataSet.getTrainingSet(excel, fractionTrainingSet);
 
 
     // TASK 4: write a new method in DataSet.java which takes as arguments to DataPoint objects,
@@ -32,7 +40,6 @@ public class kNNMain{
 
     // TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
     // point based on nearest neighbors in training set. Calculate accuracy of model.
-
 
   }
 
